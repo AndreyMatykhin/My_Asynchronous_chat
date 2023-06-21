13dd18825b8a47730e69fa319a125ec396ac8f22
Matyukhin_Andrey <matykhinand2021@gmail.com>
2023-06-22 00:32:51 +0300

    last Edition

--- lesson_16/pack_client/setup_cxfreeze.py
+++ lesson_16/pack_client/setup_cxfreeze.py
@@ -1,10 +1,18 @@
-from setuptools import setup, find_packages
+import sys
+from cx_Freeze import setup, Executable
 
-setup(name="my_messanger_client",
-      version="0.0.1",
-      description="messenger_client",
-      author="Andrey Matyukhin ",
-      author_email="matykhinand2021@gmail.com",
-      packages=find_packages(),
-      install_requires=['PyQt5', 'sqlalchemy', 'pycryptodome', 'pycryptodomex']
-      )
+build_exe_options = {
+    "packages": ['client']
+}
+setup(
+    name="my_messanger_client",
+    version="0.0.1",
+    description="messenger_client",
+    options={
+        "build_exe": build_exe_options
+    },
+    executables=[Executable('client/client.py',
+                            # base='Win32GUI',
+                            target_name='client.exe',
+                            )]
+)
--- lesson_16/pack_server/setup_cxfreeze.py
+++ lesson_16/pack_server/setup_cxfreeze.py
@@ -1,10 +1,19 @@
-from setuptools import setup, find_packages
+from cx_Freeze import setup, Executable
 
-setup(name="my_messanger_server",
-      version="0.0.1",
-      description="messenger_server",
-      author="Andrey Matyukhin ",
-      author_email="matykhinand2021@gmail.com",
-      packages=find_packages(),
-      install_requires=['PyQt5', 'sqlalchemy', 'pycryptodome', 'pycryptodomex']
-      )
+build_exe_options = {
+    "packages": ["server"],
+}
+setup(
+    name="my_messanger_server",
+    version="0.0.1",
+    description="messenger_server",
+    author="Andrey Matyukhin ",
+    author_email="matykhinand2021@gmail.com",
+    options={
+        "build_exe": build_exe_options
+    },
+    executables=[Executable('server/server.py',
+                            # base='Win32GUI',
+                            target_name='server.exe',
+                            )]
+)
